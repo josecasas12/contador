4516c2d0efa6260e493ca7eae2730140f2f0f90f
jose <josecasas.trade@gmail.com>
2025-10-23 09:24:21 -0500

    agrege un contador funcional

--- contador/src/components/Contador.jsx
+++ contador/src/components/Contador.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-
-const NombreDelComponente = () => {
-    let numero = 1;
+import React, { useState } from 'react'
+const Contador = () => {
+    const[numero, setNumero] = useState (0);
+   // let numero = 1;
     const Sumar = () => {
         numero ++;
-        console.log(numero);
+        //console.log(numero);
+        setNumero(numero + 1);
     }
   return (
     <section>
       <h1>Contador</h1>
     <div className='d-flex align-items-center justify-content-center'>
-        <button className='btn btn-primary'onClick={Sumar}> + </button>
+        <button className='btn btn-primary'onClick={()=>setNumero(numero+1)}> + </button>
         <p className='fs-3 mx-3'>{numero}</p>
-        <button className='btn btn-danger'> - </button>
+        <button className='btn btn-danger'onClick={()=>setNumero(numero-1)}> - </button>
     </div>
     </section>
   )
 }
 
-export default NombreDelComponente
+export default Contador
